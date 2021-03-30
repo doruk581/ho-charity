c0e5db42067d087ecf4cc292562d73a4eb74e7ad
Doruk <doruksucs@gmail.com>
2021-03-30 21:56:04 +0300

    Migrations added

---
+++ Ho.Charity/Migrations/20210330185417_CharityOrganization.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ho.Charity.Migrations
+{
+    public partial class CharityOrganization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CharityOrganizations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrganizationName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrganizationAuthorizedPersonEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrganizationAuthorizedPersonPhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Iban = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    OrganizationAuthorizedPersonName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OrganizationAuthorizedPersonSurname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TaxOffice = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TaxNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SubMerchantKey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MerchantId = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharityOrganizations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharityOrganizations");
+        }
+    }
+}
